bd07d589c1dccba6c985204a60da801ccd7d433d
Andreas Leibrock <git@leibi.net>
2023-01-28 10:49:21 +0100

    initial commit

---
+++ src/main/java/net/leibi/parkrunstats/ParkrunstatsApplication.java
@@ -0,0 +1,13 @@
+package net.leibi.parkrunstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkrunstatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkrunstatsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/leibi/parkrunstats/ParkrunstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.leibi.parkrunstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkrunstatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
